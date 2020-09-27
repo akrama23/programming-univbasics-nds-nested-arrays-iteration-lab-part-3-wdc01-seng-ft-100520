a476490f7e9a8e594659e15720874f453fcb5fe4
akam alam <arundelfootball23@gmail.com>
2020-09-27 19:57:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].size do 
         
         if  src[row_index][element_index].class == String  
-        print  mixed_strings += src[row_index][element_index]
+          mixed_strings += src[row_index][element_index]
       end 
          element_index += 1 
       end 
